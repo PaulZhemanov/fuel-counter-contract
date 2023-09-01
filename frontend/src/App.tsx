1777b5188635e463405fe099aa1db7b70cbd29c0
alexey <alexnagornnyy.an@gmail.com>
2023-09-01 18:01:05 +0100

    fixed example

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,16 +1,14 @@
-import React, {useEffect, useState} from "react";
+import React, { useEffect, useState } from "react";
 import "@fuel-wallet/sdk";
 import "./App.css";
 // Import the contract factory -- you can find the name in index.ts.
 // You can also do command + space and the compiler will suggest the correct name.
-import {useFuel} from "./hooks/useFuel";
-import {useIsConnected} from "./hooks/useIsConnected";
-import {Provider, Wallet} from "fuels";
-import {CounterContractAbi__factory} from "./contracts";
+import { useFuel } from "./hooks/useFuel";
+import { useIsConnected } from "./hooks/useIsConnected";
+import { CounterContractAbi__factory } from "./contracts";
 
 const NODE_URL = "https://beta-4.fuel.network/graphql";
-const PRIVATE_KEY_WITH_BALANCE = "";
-// The address of the contract deployed the Fuel testnet
+// The address of thes contract deployed the Fuel testnet
 const COUNTER_ID =
     "0xce3dddd28c0c6c1cb4a4241943dcfcad6bed6657e659f1a97a7ab2b7b49c3216";
 
@@ -21,20 +19,20 @@ function App() {
     const [isConnected] = useIsConnected();
 
     useEffect(() => {
+        update_counter()
+    }, [fuel, isConnected])
+
+    async function update_counter() {
         setLoading(true);
-        const provider = new Provider(NODE_URL);
-        const wallet = PRIVATE_KEY_WITH_BALANCE as any === ""
-            ? Wallet.generate({provider})
-            : Wallet.fromPrivateKey(PRIVATE_KEY_WITH_BALANCE, provider);
-        CounterContractAbi__factory
-            .connect(COUNTER_ID, wallet)
-            .functions
-            .count()
-            .simulate()
+        isConnected && fuel
+            .currentAccount()
+            .then((account) => fuel.getWallet(account))
+            .then((wallet) => CounterContractAbi__factory.connect(COUNTER_ID, wallet))
+            .then(instance => instance.functions.count().simulate())
             .then((res) => setCounter(res.value.toNumber()))
             .catch((e) => console.error(e))
             .finally(() => setLoading(false));
-    }, [])
+    }
 
     async function increment() {
         setLoading(true);
@@ -42,23 +40,24 @@ function App() {
             .currentAccount()
             .then((account) => fuel.getWallet(account))
             .then((wallet) => CounterContractAbi__factory.connect(COUNTER_ID, wallet))
-            .then(instance => instance.functions.increment().txParams({gasPrice: 1}).call())
+            .then(instance => instance.functions.increment().txParams({ gasPrice: 1 }).call())
+            .then(update_counter)
             .catch(e => console.error(e))
             .finally(() => setLoading(false))
     }
 
-    if (fuelLoading || loading) return  <div className="App"><h3>loading</h3></div>
+    // if (fuelLoading || loading) return <div className="App"><h3>loading</h3></div>
 
     return (
         <div className="App">
-            <h3>Counter: {counter == null ? "loading" : counter?.toFixed(0)}</h3>
-            {/*{*/}
-            {/*    isConnected ? (*/}
-            {/*        <button style={buttonStyle} onClick={increment}>Increment</button>*/}
-            {/*    ) : (*/}
-            {/*        <button style={buttonStyle} onClick={() => fuel.connect()}>Connect</button>*/}
-            {/*    )*/}
-            {/*}*/}
+            {isConnected && <h3>Counter: {counter == null ? "loading" : counter?.toFixed(0)}</h3>}
+            {
+                isConnected ? (
+                    <button style={buttonStyle} onClick={increment}>Increment</button>
+                ) : (
+                    <button style={buttonStyle} onClick={() => fuel.connect()}>Connect</button>
+                )
+            }
         </div>
     );
 }
