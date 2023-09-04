60d3137407afd612cb08bcb67c780bfd10b88a15
alexey <alexnagornnyy.an@gmail.com>
2023-09-04 18:53:27 +0100

    updated indexer

--- counter-indexer/src/lib.rs
+++ counter-indexer/src/lib.rs
@@ -3,11 +3,17 @@ use fuel_indexer_utils::prelude::*;
 
 #[indexer(manifest = "counter_indexer.manifest.yaml")]
 pub mod counter_indexer_index_mod {
-    
+
+    fn handle_block(block: BlockData) {
+        let height = block.height;
+        let txs = block.transactions.len();
+        info!("🧱 Block height: {height} | transacrions: {txs}");
+    }
+
     fn handle_increment_params(data: IncrementParams) {
         Logger::info(format!("✨ IncrementParams: {:#?}", data).as_str());
         let increment = Increment {
-            id: data.timestamp,
+            id: SizedAsciiString::try_from(data.counter.to_string()).unwrap(),
             caller: data.caller,
             counter: data.counter,
             timestamp: data.timestamp,
